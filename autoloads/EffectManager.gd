03d936c95b03b973d77bad50d4e39ae3c8ea3888
Gabriel Cancro <gabrielcancro@gmail.com>
2026-05-21 11:29:31 -0300

    update

--- autoloads/EffectManager.gd
+++ autoloads/EffectManager.gd
@@ -3,3 +3,9 @@ extends Node
 func move_to(node, position):
 	var tw = create_tween()
 	tw.tween_property(node,"position",position,0.2)
+
+func move_yoyo(node,dir):
+	var tw = create_tween()
+	var start_pos = node.position
+	tw.tween_property(node,"position",start_pos+dir,0.15)
+	tw.tween_property(node,"position",start_pos,0.15)
--- autoloads/MapManager.gd
+++ autoloads/MapManager.gd
@@ -1,46 +1,52 @@
 extends Node
 
 @onready var tilemap: TileMapLayer
-const FREE_TILES = ["a2x1"]
+const FREE_TILES = ["a1x1"]
 
 func initialize_map_manager(_tile_map):
 	tilemap = _tile_map
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		if !tilemap: return 
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
 		var mouse_pos = get_viewport().get_mouse_position()
+		var tile_pos = tilemap.local_to_map(mouse_pos)
+		is_tile_free(tile_pos.x, tile_pos.y)
+#
+#func _process(delta):
+	#if Input.is_action_just_pressed("ui_accept"):
+		#if !tilemap: return 
+		#var mouse_pos = get_viewport().get_mouse_position()
+#
+		## Convertir posición global -> celda del tilemap
+		#var cell = tilemap.local_to_map(mouse_pos)
+#
+		#print("Cell:", cell)
+#
+		## Ver si hay tile
+		#var source_id = tilemap.get_cell_source_id(cell)
+#
+		#if source_id == -1:
+			#print("No hay tile")
+			#return
+#
+		## Datos del tile
+		#var atlas_coords = tilemap.get_cell_atlas_coords(cell)
+		#var alternative = tilemap.get_cell_alternative_tile(cell)
+#
+		#print("Source:", source_id)
+		#print("Atlas:", atlas_coords)
+		#print("Alternative:", alternative)
 
-		# Convertir posición global -> celda del tilemap
-		var cell = tilemap.local_to_map(mouse_pos)
 
-		print("Cell:", cell)
-
-		# Ver si hay tile
-		var source_id = tilemap.get_cell_source_id(cell)
-
-		if source_id == -1:
-			print("No hay tile")
-			return
-
-		# Datos del tile
-		var atlas_coords = tilemap.get_cell_atlas_coords(cell)
-		var alternative = tilemap.get_cell_alternative_tile(cell)
-
-		print("Source:", source_id)
-		print("Atlas:", atlas_coords)
-		print("Alternative:", alternative)
-
-
-func cs(tx,ty):
-	var cell = tilemap.get_cell_source_id(Vector2(tx,ty))
-	var source_id = tilemap.get_cell_source_id(cell)
+func is_tile_free(tx,ty):
+	var tile_pos = Vector2(tx,ty)
+	var source_id = tilemap.get_cell_source_id(tile_pos)
 	if source_id == -1:
-		return false
 		print("No hay tile")
-		return
-	var atlas_coords = tilemap.get_cell_atlas_coords(cell)
-	var atlas_key = "a"+str(atlas_coords.x)+"x"+str(atlas_coords.x)
+		return false
+	var atlas_coords = tilemap.get_cell_atlas_coords(tile_pos)
+	var atlas_key = "a"+str(atlas_coords.x)+"x"+str(atlas_coords.y)
+	print(tile_pos,"->",atlas_key,">",source_id,": ",atlas_key in FREE_TILES)
 	if atlas_key in FREE_TILES:
 		return true
 	else:
